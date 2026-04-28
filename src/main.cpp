ef12bf76f37eab62d3390abebceb15dcad070bab
Lou CHARLET <lou.charlet@epitech.eu>
2026-04-28 21:46:13 +0200

    ui: implement letterboxing to maintain aspect ratio on resize

--- src/main.cpp
+++ src/main.cpp
@@ -26,10 +26,35 @@ struct Popup {
     Popup(const sf::Texture& texture) : sprite(texture), timer(0.f) {}
 };
 
+sf::View getLetterboxView(sf::View view, int windowWidth, int windowHeight) {
+    float windowRatio = (float)windowWidth / (float)windowHeight;
+    float viewRatio = view.getSize().x / view.getSize().y;
+    float sizeX = 1.f;
+    float sizeY = 1.f;
+    float posX = 0.f;
+    float posY = 0.f;
+
+    if (windowRatio > viewRatio) {
+        sizeX = viewRatio / windowRatio;
+        posX = (1.f - sizeX) / 2.f;
+    } else {
+        sizeY = windowRatio / viewRatio;
+        posY = (1.f - sizeY) / 2.f;
+    }
+
+    view.setViewport({posX, posY, sizeX, sizeY});
+    return view;
+}
+
 int main() {
     AssetLoader::preloadAll();
-    sf::RenderWindow window(sf::VideoMode({(unsigned int)WINDOW_WIDTH, (unsigned int)WINDOW_HEIGHT}), "Pointes of No Return");
+    sf::RenderWindow window(sf::VideoMode({(unsigned int)WINDOW_WIDTH, (unsigned int)WINDOW_HEIGHT}), "Pointes of No Return", sf::Style::Default | sf::Style::Resize);
     window.setFramerateLimit(60);
+
+    sf::View view;
+    view.setSize({WINDOW_WIDTH, WINDOW_HEIGHT});
+    view.setCenter({WINDOW_WIDTH / 2.f, WINDOW_HEIGHT / 2.f});
+    window.setView(getLetterboxView(view, (int)WINDOW_WIDTH, (int)WINDOW_HEIGHT));
 
     // Scene manager + start with main menu1 (save slots)
     SceneManager scenes;
@@ -40,6 +65,9 @@ int main() {
         float dt = clock.restart().asSeconds();
         while (auto event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>()) window.close();
+            if (const auto* resized = event->getIf<sf::Event::Resized>()) {
+                window.setView(getLetterboxView(view, (int)resized->size.x, (int)resized->size.y));
+            }
             if (scenes.current()) scenes.current()->handleEvent(event, window);
         }
 
